5d1e063bf4e40e6ce9394322740021649a012ca0
arlenye <arlen_ye@hotmail.com>
2025-08-03 16:06:37 +0800

    训练相关代码

--- codes/吸烟检测测试 yolo11n.py
+++ codes/吸烟检测测试 yolo11n.py
@@ -3,4 +3,4 @@ import os
 os.environ["KMP_DUPLICATE_LIB"] = "True"
 model = YOLO("yolo11n.yaml")
 if __name__ == "__main__" :
-    results = model.train(data = "smoke_fall.yaml", device = 0, epochs = 100, imgsz = 640)
+    results = model.train(data = "smoke_fall.yaml", epochs = 2, imgsz = 640)
